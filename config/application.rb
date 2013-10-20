3b2136e70293cd910c80ec4ff4bf9dcd5e8c7a47
saasbook <saasbook@saasbook.(none)>
2013-10-19 21:05:18 -0700

    test 3

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require "bootstrap-sass"
-require "bootstrap"
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
